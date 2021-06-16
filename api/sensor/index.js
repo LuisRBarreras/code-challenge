b3a87bcafa6bb456d89fa6ea747d446b3cc828e1
Luis Barreras <luis.barreras@unosquare.com>
2021-06-15 19:12:41 -0700

    [TASK-2]Add Eslint, and Editor configuration

--- api/sensor/index.js
+++ api/sensor/index.js
@@ -1,11 +1,9 @@
-'use strict';
-
-var express = require('express');
+const express = require('express');
 // const controller = require('./sensor.controller');
 
-var router = express.Router();
+const router = express.Router();
 router.get('/:field/:operation/:timeInterval', (req, res) => {
-    res.send('sensor endpoint');
+  res.send('sensor endpoint');
 });
 
 module.exports = router;
--- app.js
+++ app.js
@@ -2,25 +2,16 @@ const express = require('express');
 
 const app = express();
 const port = 3000;
-
-
 const prefix = 'v1';
-const routes = [
-    {path:`/${prefix}/sensor`, file:'./api/sensor' },
-];
 
-routes.forEach(({path, file}) => {
-    console.log(file, path);
-    app.use(path, require(file));
-});
+// Definition of routes
+app.use(`/${prefix}/sensor`, require('./api/sensor'));
 
-
-//Default route
+// Default route
 app.get('/', (req, res) => {
-    res.send('Running');
+  res.send('Running');
 });
-
 
 app.listen(port, () => {
-    console.log(`Application listening at http://localhost:${port}`);
+  console.log(`Application listening at http://localhost:${port}`);
 });
